2334a81fa48406c46508faf6c5a8367dd610b6f4
iheb saad <ihebsaad@gmail.com>
2025-05-06 17:28:23 +0100

    fix cats

--- resources/views/layouts/partial/head.blade.php
+++ resources/views/layouts/partial/head.blade.php
@@ -48,9 +48,7 @@
 	<link rel="stylesheet" type="text/css"    href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 	<style>
-		footer{
-			position:relative;
-		}
+ 
 	</style>
 
   	@yield('styles')
