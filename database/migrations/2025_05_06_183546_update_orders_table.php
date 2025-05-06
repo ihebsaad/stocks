3d02f9ed5e1979f70b442a76882dbebd97411fe7
iheb saad <ihebsaad@gmail.com>
2025-05-06 17:39:22 +0100

    fix orders

---
+++ database/migrations/2025_05_06_183546_update_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->decimal('subtotal', 10, 2);
+            $table->decimal('discount', 10, 2);
+            $table->decimal('delivery_cost', 10, 2);
+            $table->decimal('total', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('orders', function (Blueprint $table) {
+        $table->dropColumn('subtotal');
+        $table->dropColumn('discount');
+        $table->dropColumn('delivery_cost');
+        $table->dropColumn('total');
+        });
+
+    }
+}
