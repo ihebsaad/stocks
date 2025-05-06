b784b82f679db88e54b9474fc5a6fa50a571c5d4
iheb saad <ihebsaad@gmail.com>
2025-05-06 18:12:55 +0100

    test

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -71,18 +71,7 @@ class ProductsController extends Controller
                 ->filterColumn('stock_quantity', function($query, $keyword) {
                     $query->whereRaw("(stock_quantity + (SELECT COALESCE(SUM(quantity), 0) FROM stock_entry_items WHERE stock_entry_items.product_id = products.id)) like ?", ["%{$keyword}%"]);
                 })
-                ->setRowClass(function ($product) {
-                    $qty = StockEntryItem::where('product_id', $product->id)->sum('quantity');
-                    $qty += $product->stock_quantity;
-                    
-                    if ($product->min_qty > 0 && $product->min_qty >= $qty) {
-                        return 'bg-danger';
-                    } elseif ($product->min_qty > 0 && ($qty - $product->min_qty) < 6) {
-                        return 'bg-warning';
-                    }
-                    
-                    return '';
-                })
+
                 ->editColumn('description', function ($product) {
                     return nl2br($product->description);
                 })
