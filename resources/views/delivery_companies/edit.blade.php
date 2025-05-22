8e498ebdc4fed18bb32b3a8bbe8bec212114acbf
iheb saad <ihebsaad@gmail.com>
2025-05-22 20:44:46 +0100

    details apis

--- resources/views/delivery_companies/edit.blade.php
+++ resources/views/delivery_companies/edit.blade.php
@@ -175,4 +175,22 @@
         </div>
     </div>
 </div>
+
+<script>
+function togglePassword(fieldId) {
+    const field = document.getElementById(fieldId);
+    const icon = document.getElementById('toggleIcon');
+    
+    if (field.type === 'password') {
+        field.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        field.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
+</script>
+
 @endsection
