2faba25bbbad2088a6a73840f72a6f24ef918b0b
iheb saad <ihebsaad@gmail.com>
2025-05-29 16:40:57 +0100

    test

--- resources/views/parcels/show.blade.php
+++ resources/views/parcels/show.blade.php
@@ -420,7 +420,9 @@
                         </span>
                     </div>
                     <table class="table table-striped mt-4" style="width:100%;text-align:center">
+                        <thead>
                         <tr><th>DESIGNATION</th><th>PU</th><th>QTE</th><th>TOT</th></tr>
+                        </thead>
                         @foreach($parcel->order->items as $index => $item)
                         <tr><td>{{$item->product->name}} </td><td>{{$item->unit_price}}</td><td>{{$item->quantity}}</td><td>{{ number_format($item->unit_price * $item->quantity, 2) }} <sup>TND</sup></td></tr>
                         @endforeach
