1c29886228f47dac54fe906cda71ad9bc466641e
iheb saad <ihebsaad@gmail.com>
2025-05-29 22:34:09 +0100

    BL

--- resources/views/bl/template.blade.php
+++ resources/views/bl/template.blade.php
@@ -349,74 +349,79 @@
                 <td style="width:50%">
                     <div class="section-header">Expéditeur</div>
                         <div class="section-content">
-                        <table>
-                            <tr>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">Nom:</span>
-                                        <span class="info-value">Z&A Home</span>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">Adr:</span>
-                                        <span class="info-value">Ksibet médiouni</span>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">Tél:</span>
-                                        <span class="info-value">55 969 997</span>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">MF:</span>
-                                        <span class="info-value">1768373/Z/P/M/000</span>
-                                    </div>
-                                </td>
-                            </tr>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">Nom:</span>
+                                            <span class="info-value">Z&A Home</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">Adr:</span>
+                                            <span class="info-value">Ksibet médiouni</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">Tél:</span>
+                                            <span class="info-value">55 969 997</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">MF:</span>
+                                            <span class="info-value">1768373/Z/P/M/000</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </td>
                 <td style="width:50%" >
-                        <div class="section-header">Transporteur</div>
-                        <table>
-                            <tr rowspan="3">
-                                <td>
-                                    <div class="transporteur-section">
-                                        <span class="company-badge bg-{{ $parcel->company->id }}">
-                                            {{ strtoupper($parcel->company->name) }}
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">Tél:</span>
-                                        <span class="info-value">{{$parcel->company->phone}}</span>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">Adr:</span>
-                                        <span class="info-value">{{$parcel->company->phone}}</span>
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="info-line">
-                                        <span class="info-label">MF:</span>
-                                        <span class="info-value">{{$parcel->company->phone}}</span>
-                                    </div>
-                                </td>
-                            </tr>
-                        </table>
+                    <div class="section-header">Transporteur</div>
+                        <div class="section-content">
+                            <table>
+                                <tr rowspan="3">
+                                    <td>
+                                        <div class="transporteur-section">
+                                            <span class="company-badge bg-{{ $parcel->company->id }}">
+                                                {{ strtoupper($parcel->company->name) }}
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">Tél:</span>
+                                            <span class="info-value">{{$parcel->company->phone}}</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">Adr:</span>
+                                            <span class="info-value">{{$parcel->company->phone}}</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <div class="info-line">
+                                            <span class="info-label">MF:</span>
+                                            <span class="info-value">{{$parcel->company->phone}}</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </td>
             </tr>
         </table>
