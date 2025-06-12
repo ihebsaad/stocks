8c953f68410491eb8fac496f76a6700a8899bb2b
iheb saad <ihebsaad@gmail.com>
2025-06-12 21:08:24 +0100

    filtre par date parcels

--- resources/views/parcels/index.blade.php
+++ resources/views/parcels/index.blade.php
@@ -276,7 +276,7 @@ $(function() {
         }
     }
 
-    $('#delivery-company-filter , #date_from , #date_to').change(function() {
+    $('#delivery-company-filter , #date_from , #date_to').on('change', function() {
         alert();
         table.draw();
     });
