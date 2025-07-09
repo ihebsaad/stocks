7cbb2948dd02d134664a7ef71f7e40f59f4fec3b
iheb saad <ihebsaad@gmail.com>
2025-07-09 21:26:20 +0100

    stats

--- resources/views/parcels/stats.blade.php
+++ resources/views/parcels/stats.blade.php
@@ -127,7 +127,6 @@
 
   
     @section('content')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
