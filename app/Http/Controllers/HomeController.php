2670e362ac89469e8f2b2f551e78c1cf9f0cf66c
iheb saad <ihebsaad@gmail.com>
2025-07-13 11:14:46 +0100

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Invoice;
-use App\Models\Customer;
-use App\Models\Item;
 use App\Models\Order;
 use App\Models\Parcel;
 use App\Models\Product;
-use App\Models\Reglement;
 use Carbon\Carbon;
 use DB;
 
@@ -42,24 +38,4 @@ class HomeController extends Controller
     }
 
 
-    public function getInvoicesByProduct($productId)
-    {
-        // Récupérer les factures contenant le produit spécifié
-        $invoices = Invoice::whereHas('items', function ($query) use ($productId) {
-            $query->where('product', $productId);
-        })->with(['customer', 'items'])->get();
-
-        // Formater les données pour la réponse JSON
-        $data = $invoices->map(function ($invoice) {
-            $customer=Customer::find($invoice->customer);
-            return [
-                'invoice_number' => $invoice->id,
-                'date' => $invoice->created_at->format('d/m/Y'),
-                'customer' => $customer->name.' '.$customer->lastname,
-                'total_ttc' => $invoice->total_ttc,
-            ];
-        });
-
-        return response()->json($data);
-    }
 }
