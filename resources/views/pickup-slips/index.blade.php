bef54a607b9cbd51d8d27bbd98debe1c16ae1a91
iheb saad <ihebsaad@gmail.com>
2025-07-15 22:42:03 +0100

    pickups

--- resources/views/pickup-slips/index.blade.php
+++ resources/views/pickup-slips/index.blade.php
@@ -154,7 +154,7 @@ $(document).ready(function() {
         language: {
             processing: "Traitement en cours...",
             search: "Rechercher&nbsp;:",
-            lengthMenu: "Afficher _MENU_ &éléments",
+            lengthMenu: "Afficher _MENU_ &nbsp;éléments",
             info: "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
             infoEmpty: "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
             infoFiltered: "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
