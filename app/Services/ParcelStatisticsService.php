7e956dbe4490bda407fbc895df6b57ca6699788e
iheb saad <ihebsaad@gmail.com>
2025-07-26 12:03:40 +0100

    stats

--- app/Services/ParcelStatisticsService.php
+++ app/Services/ParcelStatisticsService.php
@@ -309,7 +309,7 @@ class ParcelStatisticsService
             case 'daily':
                 return Carbon::parse($date)->format('d/m');
             case 'weekly':
-                return 'S' ;
+                return 'S ' .$date->format("W");
             case 'monthly':
                 return Carbon::parse($date)->format('m/Y');
             default:
