4651d8ded9b7ba73af85cdbd0322d0215858d900
iheb saad <ihebsaad@gmail.com>
2025-09-23 13:19:52 +0100

    paysage

--- app/Http/Controllers/ParcelController.php
+++ app/Http/Controllers/ParcelController.php
@@ -329,7 +329,7 @@ class ParcelController extends Controller
         $pdf = Pdf::loadView('bl.template', compact('parcel', 'expediteur','barcode'));
         
         // Configurer le PDF pour format ticket (A5)
-        $pdf->setPaper('A5', 'portrait');
+        $pdf->setPaper('A5', 'paysage');
         
         // Retourner le PDF
         return $pdf->stream('BL-' . $parcel->reference . '.pdf');
