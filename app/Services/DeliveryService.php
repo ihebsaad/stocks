359a650fd9352766fefe8c222f3822965d9930ac
iheb saad <ihebsaad@gmail.com>
2025-10-08 22:43:10 +0100

    coliexpress get single last

--- app/Services/DeliveryService.php
+++ app/Services/DeliveryService.php
@@ -88,7 +88,7 @@ class DeliveryService
     public function syncParcelStatuses()
     {
         
-        if (strtolower($this->company->name) === 'droppex'  ) {
+        if (strtolower($this->company->name) === 'droppex' && (false) ) {
             //$currentDate = now();
             
             // 🎯 Droppex → pas de /list, on doit faire un get par colis
