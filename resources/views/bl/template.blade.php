68b18c54a561172a1022dc46b4306056366bf141
iheb saad <ihebsaad@gmail.com>
2025-10-16 11:44:26 +0100

    updates

--- resources/views/bl/template.blade.php
+++ resources/views/bl/template.blade.php
@@ -348,8 +348,8 @@
                                 <tr >
                                     <td style="width:40%">                                        
                                         <div class="transporteur-section">
-                                            @if($parcel->company->id==2)  <img src="{{ asset('img/droppex.png')}}" style="width:100px" />@endif
-                                            @if($parcel->company->id==3)  <img src="{{ asset('img/coliexpressb.png')}}" style="width:100px" />@endif
+                                            @if($parcel->company->id==2)  <img src="{{ asset('img/droppexblack.jpg')}}" style="width:100px" />@endif
+                                            @if($parcel->company->id==3)  <img src="{{ asset('img/coliexpressblack.png')}}" style="width:100px" />@endif
                                             <!--<span class="company-badge bg-{{ $parcel->company->id }}">
                                                 {{ strtoupper($parcel->company->name) }}
                                             </span>-->
